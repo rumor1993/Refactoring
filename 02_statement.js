1251d6ab42bacba06314b9c0c093ccd8c3545058
rumor1993 <rumor1993@naver.com>
2021-01-28 01:32:01 +0900

    volumeCredits 변수 제거

--- 02_statement.js
+++ 02_statement.js
@@ -12,24 +12,8 @@ function statement(invoices, plays) {
 
     for (let perf of invoices.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
+        let thisAmount = amountFor(perf, play);
 
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience -30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience -20);
-                }
-                break;
-            default:
-                throw new Error(`알 수 없는 장르 : ${play.type}`);
-            }
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 희극 관객 5명마다 추가 포인트를 제공한다.
@@ -41,4 +25,30 @@ function statement(invoices, plays) {
     result += `총액 : ${format(totalAmount/100)} \n`
     result += `적립 포인트 : ${volumeCredits} 점 \n`
     return result;
+
+    
+    function amountFor(aPerformance, play) {
+        let result = 0;
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience -30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience -20);
+                }
+                break;
+            default:
+                throw new Error(`알 수 없는 장르 : ${play.type}`);
+            }
+        return result;
+    }
 }
+
+
+
+
